201df2dc3d28d859072edf0e1b695a239a1dbf2e
CrocodielRussian <mk-spbkiselev@yandex.ru>
2024-07-11 12:25:38 +0300

    feat: done look ahead

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,19 @@
+open Option
 let is_alpha = function 'a' .. 'z' | 'A' .. 'Z' -> true | _ -> false
 let is_digit = function '0' .. '9' -> true | _ -> false
 let is_whitespace = function ' ' -> true | _ -> false
 
+let text = "   10000"
 let int_option_to_string (i : int option) : string =
   match i with Some x -> string_of_int x | None -> "None"
+type oper = Plus | Multiply | Divide | Minus | Invalid
+type expr =
+| Number of int
+| Unary of oper * expr
+| Binary of expr * oper * expr
+| Error
 
-let text = "                   10"
+
 let pos = ref 0
 let length = String.length text
 
@@ -21,10 +29,26 @@ let number =
     acc := !acc ^ String.make 1 text.[!pos];
     incr pos
   done;
-  if String.length !acc > 0 then Some (int_of_string !acc) else None;;
+  if String.length !acc > 0 
+  then Number(int_of_string !acc)
+  else Error;;
 
-number |> int_option_to_string  |> print_endline;;
 
-pos := 0;;
+let num = Number 10;;
 
-number |> int_option_to_string  |> print_endline;;
+let op = function
+  | '+' -> Plus
+  | '-' -> Minus
+  | '*' -> Multiply
+  | '/' -> Divide
+  | _ -> Invalid
+
+let rec parse_expr text =
+  ws();
+  let head = number in 
+  let operation = op text.[!pos] in
+
+  if operation != Invalid
+    then Binary(head, operation, parse_expr text)
+  else
+    head
