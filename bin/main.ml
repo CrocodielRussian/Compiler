16831c570f4101ac47cc9770caffab1f7e0c5704
CrocodielRussian <mk-spbkiselev@yandex.ru>
2024-07-12 13:27:33 +0300

    feat: done while-loop

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,6 @@
 module Main = struct
   let text = "a:=10+11+10;"
   let pos = ref 0
-  let res = Compiler.StatementParser.stmt text pos
-
-  let () =
-    Compiler.Parser.StatementParser.stmt_to_string text pos res |> print_endline
+  let res = Compiler.Parser.asign_stmt text pos
+  let () = Compiler.Parser.stmt_to_string text pos res |> print_endline
 end
