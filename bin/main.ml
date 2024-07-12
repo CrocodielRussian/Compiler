142724590a2d112f7fcec8555e53471e15934fad
CrocodielRussian <mk-spbkiselev@yandex.ru>
2024-07-12 10:39:09 +0300

    feat: Parser.ml without module

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,6 @@
-include Compiler.Parser.Parser
-
 module Main = struct
   let text = "10+11+10"
   let pos = ref 0
-  let res = Compiler.Parser.Parser.parse_expr text pos
-  let () = Compiler.Parser.Parser.expr_to_string text pos res |> print_endline
+  let res = Compiler.Parser.parse_expr text pos
+  let () = Compiler.Parser.expr_to_string text pos res |> print_endline
 end
