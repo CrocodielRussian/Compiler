350a4d53210cea08ec21a79ddbb353c060fd8edd
IliaSuponeff <90147482+IliaSuponeff@users.noreply.github.com>
2024-07-21 20:54:09 +0300

    feat: add support of if statement of translation to assembler

--- bin/main.ml
+++ bin/main.ml
@@ -89,7 +89,7 @@ module Main = struct
         in
         cur_stack_pointer := !cur_stack_pointer - 4;
         full_asm ^ "\n" ^ binop_to_asm op
-    | EmptyExpression -> ""
+    | EmptyExpression -> "# Empty expression"
 
   let parse_of_condition e cur_stack_pointer count_of_while =
     match e with
@@ -126,46 +126,92 @@ module Main = struct
         | _ -> failwith "TO DO")
     | _ -> failwith "TO DO"
 
-  let rec stmt_to_asm s cur_stack_pointer count_of_while =
+  let rec stmt_to_asm s cur_stack_pointer count_of_while count_of_if
+      open_label_count =
     match s with
-    | Expression e1 -> (
-        match e1 with
-        | AssignExpression (_, _, _) -> expr_to_asm e1 cur_stack_pointer
-        | _ -> "")
-    | AssignStatement (v, e1) ->
-        (* var <v> = <e1>*)
+    | Expression ex -> (
+        match ex with
+        | AssignExpression (_, _, _) -> expr_to_asm ex cur_stack_pointer
+        | _ ->
+            failwith
+              ("ASTError: unsupported  expression statement: "
+              ^ string_of_expression ex ex ex
+              ^ ";."))
+    | AssignStatement (v, ex) ->
+        (* var <v> = <ex>*)
         let var_pos = StringMap.find v !variables_shifts in
-        let asm = expr_to_asm e1 cur_stack_pointer in
+        let asm = expr_to_asm ex cur_stack_pointer in
         asm ^ "\nsw a5, -" ^ string_of_int var_pos ^ "(s0)"
-    | While (e1, stmt1) ->
-        while_loop_to_asm e1 stmt1 cur_stack_pointer count_of_while
-    | If (e1, _, _) -> expr_to_asm e1 cur_stack_pointer
-    | _ -> failwith "TO DO"
+    | While (ex, stmts) ->
+        while_loop_to_asm ex stmts cur_stack_pointer count_of_while count_of_if
+          open_label_count
+    | If (ex, then_stmts, else_stmts) ->
+        if_stmt_to_asm ex then_stmts else_stmts cur_stack_pointer count_of_while
+          count_of_if open_label_count
+    | EmptyStatement -> "# Empty Statement"
 
-  and while_loop_to_asm e stmt cur_stack_pointer count_of_while =
+  and while_loop_to_asm e stmts cur_stack_pointer count_of_while count_of_if
+      open_label_count =
     count_of_while := !count_of_while + 1;
     "j .while_condition_"
     ^ string_of_int !count_of_while
     ^ "\n\n" ^ ".while_loop_"
     ^ string_of_int !count_of_while
     ^ ":\n"
-    ^ stmts_to_asm EmptyExpression stmt cur_stack_pointer count_of_while
+    ^ stmts_to_asm stmts cur_stack_pointer count_of_while count_of_if
+        open_label_count
     ^ "\n\n.while_condition_"
     ^ string_of_int !count_of_while
     ^ ":\n"
     ^ parse_of_condition e cur_stack_pointer count_of_while
 
-  and stmts_to_asm expression statements cur_stack_pointer count_of_while =
-    let e1 = expr_to_asm expression cur_stack_pointer in
-    List.fold_left
-      (fun acc stmt -> acc ^ stmt_to_asm stmt cur_stack_pointer count_of_while)
-      e1 statements
+  and stmts_to_asm stmts cur_stack_pointer count_of_while count_of_if
+      open_label_count =
+    let stmts_asm = ref "" in
+    List.iter
+      (fun stmt ->
+        stmts_asm :=
+          !stmts_asm
+          ^ stmt_to_asm stmt cur_stack_pointer count_of_while count_of_if
+              open_label_count)
+      stmts;
+    !stmts_asm
 
-  (* "var a := 1; var b := 2; while 10 < 20 do 10 + 12; done var c := 3;" *)
+  and if_stmt_to_asm ex then_stmts else_stmts cur_stack_pointer count_of_while
+      count_of_if open_label_count =
+    incr count_of_if;
+    incr open_label_count;
+    let current_open_label_index = !count_of_if in
+    let current_if_index = !count_of_if in
+    let ex_asm = expr_to_asm ex cur_stack_pointer in
+    let then_stmts_asm =
+      stmts_to_asm then_stmts cur_stack_pointer count_of_while count_of_if
+        open_label_count
+    in
+    let else_stmts_asm =
+      stmts_to_asm else_stmts cur_stack_pointer count_of_while count_of_if
+        open_label_count
+    in
+    let else_branch_label_name =
+      ".if_" ^ string_of_int current_if_index ^ "_else"
+    in
+    let next_open_label_name = ".L" ^ string_of_int current_open_label_index in
+    ex_asm ^ "\nben a5, zero, " ^ else_branch_label_name ^ "\n" ^ then_stmts_asm
+    ^ "\nj " ^ next_open_label_name ^ "\n\n" ^ else_branch_label_name ^ ":\n"
+    ^ else_stmts_asm ^ "\nj " ^ next_open_label_name ^ "\n\n"
+    ^ next_open_label_name ^ ":"
+
+  (* let text = "var a := 1; var b := 2; while 10 < 20 do 10 + 12; done var c := 3;" *)
+
+  (* let text = "var n := 1; while n < 2 do n := n + -1; done" *)
+
+  (* let text =
+     "var n := 1; if n < 2 then n := n + -1; else n := n - 1; endif if n < 2 \
+      then n := n + 1; else n := n * -1; endif" *)
 
   let text =
-    "var n := 1; while n < 2 do n := n + 1; done while n < 2 do n := n + 1; \
-     done"
+    "var n := 0; if n then if n < 10 then n := n + 10; endif  else n := n * \
+     -1; endif"
 
   let pos = ref 0
   let shift = ref 0
@@ -173,30 +219,36 @@ module Main = struct
   let cur_stack_pointer = ref 16
   let st_stack_pointer = ref 16
   let count_of_while = ref 0
+  let count_of_if = ref 0
+  let open_label_count = ref 0
 
   let () =
     let statements = parse_program text pos in
     variables_shifts := init_variables st_stack_pointer statements;
 
     (* StringMap.iter
-       (fun key value -> print_endline (key ^ ": " ^ string_of_int value))
-       !variables_shifts; *)
+         (fun key value -> print_endline (key ^ ": " ^ string_of_int value))
+         !variables_shifts;
 
-    (* print_endline "======="; *)
+       print_endline "======="; *)
     print_endline
       ".global _start\n\
        _start:\n\
        addi sp, sp, -32\n\
        sd s0, 24(sp)\n\
-       addi s0, sp, 32";
+       addi s0, sp, 32\n\
+       # START CODE";
     cur_stack_pointer := !st_stack_pointer;
     List.iter
       (fun stmt ->
-        stmt_to_asm stmt cur_stack_pointer count_of_while |> print_endline)
+        stmt_to_asm stmt cur_stack_pointer count_of_while count_of_if
+          open_label_count
+        |> print_endline)
       statements
 
   let end_call =
-    print_endline "addi sp, sp, 32\nli a0, 0\nmv a5, a0\nli a7, 93\necall"
+    print_endline
+      "# END CODE\naddi sp, sp, 32\nli a0, 0\nmv a5, a0\nli a7, 93\necall"
 end
 
 (* let file = "bin/first.s"
