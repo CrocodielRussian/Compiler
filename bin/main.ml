8688736b7cafdfbbd19570c47321c4e0c07dfba8
IliaSuponeff <90147482+IliaSuponeff@users.noreply.github.com>
2024-07-30 17:26:41 +0300

    feat: add func-call optimization

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,5 @@
 open Compiler.Parser
-
-(* open Compiler.Optimizator *)
+open Compiler.Optimizator
 open Compiler.Asm_tree
 open Compiler.Riscv_translator
 
@@ -12,18 +11,30 @@ module Main = struct
 
   let text =
     {|
-  def main() {
-    var a := read_int();
-    var b := read_int();
-    print_int(a + b);
-    return 0;
-  }
+    def medisum(a, b) {
+      return (a + b) / 2;
+    }
 
-  |}
+    def main() {
+      23+-12;
+      var a := 10 + -0;
+      a := medisum(read_int() + (a := read_int()), -a + read_int()) + read_int();
+      var b := 10 + print_int(a);
+      print_int(a + b * (2 + 9));
+      b + -a + read_int();
+
+      if read_int() > 10 then print_int(0); else print_int(1); endif
+      while read_int() > 10 do <stmts> done
+      
+      while true do if (read_int() > 10) then <stmts> else break endif
+      return 0 + 10;
+    }
+    |}
 
   let compile out_filename =
     let content =
-      parse_program text |> program_to_asm_tree |> string_of_instr_list
+      parse_program text |> optimize_ast |> program_to_asm_tree
+      |> string_of_instr_list
     in
     append_to_file out_filename content
 
